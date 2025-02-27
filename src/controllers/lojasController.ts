aa29fbea0808d331a4f46ec62189e81419756378
Lucas <lucashenrique4848@gmail.com>
2025-02-27 14:29:41 -0300

    adicionado utils/geoUtils.ts e database modificada

--- src/controllers/lojasController.ts
+++ src/controllers/lojasController.ts
@@ -2,15 +2,15 @@ import { Request, Response } from "express";
 import dbPromise from "../dbconfig/db";
 
 export const adicionarLoja = async (req: Request, res: Response) => {
-    const { nome, cep, rua, numero, cidade } = req.body;
+    const { nome, cep, numero } = req.body;
 
     try {
         // TODO: obter latitude e longitude do CEP, adicionar em const aqui e adicionar depois no db.run.
 
         const db = await dbPromise;
         db.run(
-            "INSERT INTO lojas (nome, cep, rua, numero, cidade) VALUES (?, ?, ?, ?, ?)",
-            [nome, cep, rua, numero, cidade]
+            "INSERT INTO lojas (nome, cep, numero) VALUES (?, ?, ?)",
+            [nome, cep, numero]
         );
 
         res.status(201).send("loja adicionada com sucesso");
