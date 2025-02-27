c582846265e85765d4aab5d3e3d6d8cca00d0f1b
Lucas <lucashenrique4848@gmail.com>
2025-02-27 17:12:54 -0300

    adiciona funcao de calcular latitude e longitude do cep

--- src/utils/geoUtils.ts
+++ src/utils/geoUtils.ts
@@ -1,4 +1,6 @@
-export function distanciaHaversine(lat1: number, lon1: number, lat2: number, lon2: number) {
+import axios from "axios";
+
+export const distanciaHaversine = async (lat1: number, lon1: number, lat2: number, lon2: number) => {
     const R = 6371;
     const dLat = (lat2 - lat1) * (Math.PI / 180);
     const dLon = (lon2 - lon1) * (Math.PI / 180);
@@ -8,4 +10,16 @@ export function distanciaHaversine(lat1: number, lon1: number, lat2: number, lon
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
     return R * c;
-}
+};
+
+export const calcularLatLonDeCEP = async (cep: string) => {
+    const viaCepRes = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+    const { localidade, uf, logradouro } = viaCepRes.data;
+
+    const nominatimRes = await axios.get(
+        `https://nominatim.openstreetmap.org/search?format=json&q=${logradouro},${localidade},${uf}`
+    );
+
+    const { lat, lon } = nominatimRes.data[0];
+    return { latitude: parseFloat(lat), longitude: parseFloat(lon) }
+};
