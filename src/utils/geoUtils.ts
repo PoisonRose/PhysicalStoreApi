ab58cc8c5c9501acf2c7e634d4a7d82d2c4e0b19
Lucas <lucashenrique4848@gmail.com>
2025-02-28 15:46:08 -0300

    adiciona funcao de buscar lojas num raio de 100km

--- src/utils/geoUtils.ts
+++ src/utils/geoUtils.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const distanciaHaversine = async (lat1: number, lon1: number, lat2: number, lon2: number) => {
+export function distanciaHaversine(lat1: number, lon1: number, lat2: number, lon2: number) {
     const R = 6371;
     const dLat = (lat2 - lat1) * (Math.PI / 180);
     const dLon = (lon2 - lon1) * (Math.PI / 180);
